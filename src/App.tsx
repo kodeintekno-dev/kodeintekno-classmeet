721ff7ccbf211fb5d63528e82be2ba3534eda8ed
Mulky Ayyubi <170484966+MulkyAyyubi@users.noreply.github.com>
2025-06-25 13:13:57 +0700

    feat: make section "Tentang Kami"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <main className="container mx-auto sm:px-12 px-5">
       <Navbar />
+      <About />
     </main>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+    <section className="flex flex-col lg:flex-row gap-10 items-center px-4 md:px-10 xl:px-16 py-4 md:py-10 xl:py-16 bg-white border border-black/10 rounded-lg mt-40">
+      <div className="flex-1 px-4 md:px-10 xl:px-16 py-4 md:py-10 xl:py-16 text-center md:text-left">
+        <h1 className="text-xl md:text-3xl xl:text-6xl font-bold md:font-semibold mb-4">
+          Tentang Kami
+        </h1>
+        <p className="text-black/60 font-medium text-md xl:text-xl">
+          Kodeintekno adalah mitra strategis Anda dalam transformasi digital.
+          Kami menghadirkan solusi-solusi inovatif yang menggabungkan
+          kreativitas dan teknologi terkini. Dengan portofolio layanan lengkap
+          mulai dari pengembangan website, desain grafis, hingga pemasaran
+          digital, kami membantu bisnis Anda mencapai pertumbuhan yang
+          signifikan dan berkelanjutan.
+        </p>
+      </div>
+      <div className=" bg-gray-100 px-5 py-5 rounded-lg flex-1">
+        <div className="flex gap-2">
+          <img src="public/img/logoAbout.svg" alt="logo" />
+          <p className="font-semibold">Aktifitas Tim Kami</p>
+        </div>
+        <img className="mt-3" src="public/img/aktifitas.jpg" alt="photo" />
+      </div>
+    </section>
+  );
+}
