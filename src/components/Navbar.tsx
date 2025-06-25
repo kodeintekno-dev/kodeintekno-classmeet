adc1ad144e5cd381e7b8871bdc12079f76f28cd5
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-06-25 20:09:03 +0700

    feat: page will dim and unscrollable when hamburger opened

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { links } from "../data/navbar";
 import { twMerge } from "tailwind-merge";
 
 export default function Navbar() {
   const [navHamburger, setNavHamburger] = useState(false);
+  useEffect(() => {
+    if (navHamburger) document.body.classList.toggle("overflow-hidden");
+    return () => document.body.classList.remove("overflow-hidden");
+  }, [navHamburger]);
+
   return (
-    <div className="fixed top-0 inset-x-0 container mx-auto sm:px-12 px-5 space-y-5 z-50">
-      <nav className="flex justify-between items-center px-10 py-5 border border-black/10 rounded-lg mt-10 bg-white/80 backdrop-blur-xl">
+    <div className="fixed top-0 inset-x-0 container mx-auto sm:px-12 px-5 space-y-5">
+      <nav className="flex justify-between items-center px-10 py-5 border border-black/10 rounded-lg mt-10 bg-white/80 backdrop-blur-xl relative z-50">
         <img src="/img/logo.png" alt="KodeinTekno" className="h-6 sm:h-8" />
         <ul className="hidden lg:flex gap-8 items-center">
           {links.map((link, i) => (
@@ -40,7 +45,7 @@ export default function Navbar() {
       <ul
         className={twMerge(
           navHamburger ? "lg:hidden translate-x-0" : "translate-x-2/1",
-          "flex flex-col gap-4 items-center bg-white px-10 py-5 border border-black/10 rounded-lg transition-transform duration-300",
+          "flex flex-col gap-4 items-center bg-white px-10 py-5 border border-black/10 rounded-lg transition-transform duration-300 relative z-50",
         )}
       >
         {links.map((link, i) => (
@@ -55,6 +60,7 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
+      {navHamburger && <div className="fixed inset-0 bg-black/30 z-40" />}
     </div>
   );
 }
