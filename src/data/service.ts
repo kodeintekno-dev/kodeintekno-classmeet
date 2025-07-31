b3eaa7be6937305033011ce55d681f57fac83110
Mulky Ayyubi <170484966+MulkyAyyubi@users.noreply.github.com>
2025-07-31 15:10:13 +0700

    feat: change all lorem ipsums

--- src/data/service.ts
+++ src/data/service.ts
@@ -2,41 +2,41 @@ export const service = [
   {
     icon: "/img/icon/ux.svg",
     title: "UI/UX Design",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque",
+    desc: "Kami merancang antarmuka digital yang tidak hanya menarik secara visual, tetapi juga mudah digunakan oleh semua kalangan.",
   },
   {
     icon: "/img/icon/edu.svg",
     title: "Edu Center",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque",
+    desc: "Kami menyediakan kelas online, bimbingan mentor, dan sertifikat yang mendukung pengembangan skill digital Anda.",
   },
   {
     icon: "/img/icon/content.svg",
     title: "Content Creation",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque",
+    desc: "Dari ide hingga eksekusi, kami siap menjadi mitra Anda dalam menghasilkan konten berkualitas tinggi.",
   },
   {
-    icon: "/img/icon/dummy.svg",
-    title: "???",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque",
+    icon: "/img/icon/mobile.svg",
+    title: "Mobile App",
+    desc: "Kami mengembangkan aplikasi mobile berbasis Android dan iOS yang cepat, ringan, dan sesuai kebutuhan bisnis Anda.",
   },
   {
     icon: "/img/icon/web.svg",
     title: "Web Development",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque",
+    desc: "Kami membangun website yang responsif, cepat, dan aman untuk mendukung kebutuhan bisnis Anda.",
   },
   {
     icon: "/img/icon/market.svg",
     title: "Digital Marketing",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque",
+    desc: "Layanan kami mencakup SEO, iklan digital, dan strategi sosial media untuk meningkatkan visibilitas bisnis Anda.",
   },
   {
     icon: "/img/icon/3d.svg",
     title: "3D Design",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque",
+    desc: "Dapatkan konsep visual interaktif dalam bentuk 3D yang cocok untuk promosi dan presentasi bisnis.",
   },
   {
     icon: "/img/icon/graphic.svg",
     title: "Graphic Design",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum scelerisque",
+    desc: "Kami menciptakan desain visual yang kuat untuk logo, brosur, banner, dan materi branding lainnya yang konsisten dan menarik perhatian.",
   },
 ];
